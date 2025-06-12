27919d6661703ae9eedd17dd3216269f9650b080
knowak30 <beeeee16@gmail.com>
2025-06-12 09:29:13 +0100

    Update pagespeed.js

--- api/pagespeed.js
+++ api/pagespeed.js
@@ -1,14 +1,17 @@
-const fetch = require('node-fetch');
+export default async function handler(req, res) {
+  const { url } = req.query;
 
-module.exports = async (req, res) => {
-  const url = req.query.url;
+  if (!url) {
+    return res.status(400).json({ error: 'Brak parametru URL' });
+  }
+
   const apiUrl = `https://www.googleapis.com/pagespeedonline/v5/runPagespeed?url=${encodeURIComponent(url)}&strategy=mobile&key=AIzaSyAw0FHoSCdjUKsqAvazQIqhL7Pik2ezOEQ`;
 
   try {
     const response = await fetch(apiUrl);
     const data = await response.json();
     res.status(200).json(data);
-  } catch (err) {
-    res.status(500).json({ error: 'Błąd API PageSpeed', details: err.message });
+  } catch (error) {
+    res.status(500).json({ error: 'Błąd API', details: error.message });
   }
-};
+}

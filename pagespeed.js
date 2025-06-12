a12ebdefde0e6317dec23a2296009bb13ec19d99
knowak30 <beeeee16@gmail.com>
2025-06-12 09:14:24 +0100

    Add files via upload

---
+++ pagespeed.js
@@ -0,0 +1,15 @@
+
+const fetch = require('node-fetch');
+
+module.exports = async (req, res) => {
+  const url = req.query.url;
+  const apiUrl = `https://www.googleapis.com/pagespeedonline/v5/runPagespeed?url=${encodeURIComponent(url)}&strategy=mobile&key=AIzaSyAw0FHoSCdjUKsqAvazQIqhL7Pik2ezOEQ`;
+
+  try {
+    const response = await fetch(apiUrl);
+    const data = await response.json();
+    res.status(200).json(data);
+  } catch (err) {
+    res.status(500).json({ error: 'Błąd API PageSpeed', details: err.message });
+  }
+};
